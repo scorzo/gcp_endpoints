cb580519604bd71f2d1132d4b84b8fd066e050d6
scorzo <seancorzo@gmail.com>
2023-01-23 09:42:17 -0800

    updated proto file paths

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "GCP_endpoints/myproto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.MyServiceServer
+}
+
+func (s *server) MyMethod(ctx context.Context, in *pb.MyRequest) (*pb.MyResponse, error) {
+	log.Printf("Received request: a = %d, b = %s", in.A, in.B)
+	return &pb.MyResponse{Success: true, Message: "Success"}, nil
+}
+
+func (s *server) mustEmbedUnimplementedMyServiceServer() {}
 
 func main() {
-	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMyServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
